2351746067e258a2e82f10f6be3e2d3e6d2e1922
DarBaala <joker.milan1997gmail.com>
2022-08-12 13:50:30 +0300

    fix ts

--- src/context.ts
+++ src/context.ts
@@ -1,5 +1,5 @@
 import React from "react";
 import { EmailState } from "./pages/Login";
-const AppContext = React.createContext<EmailState>({});
+const AppContext = React.createContext({} as EmailState);
 
 export default AppContext;
